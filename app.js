d1f83b85bc209980fad10a8f00c9bad6ee7841b4
Theu <info@matteocontini.com>
2019-01-24 22:54:06 +0100

    created first server

---
+++ app.js
@@ -0,0 +1,15 @@
+const logger = require('koa-logger');
+const Koa = require('koa'); 
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+app.use(logger());
+
+router.get('/', (ctx, next) => {
+    ctx.body = 'Hello Fasters!';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3000);
